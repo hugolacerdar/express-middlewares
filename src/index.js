0f67f9f1bfe60883244a3ea403fb55654ef33b03
Hugo <hugolacerdar@gmail.com>
2021-09-15 08:33:22 -0300

    Add findUserById middleware

--- src/index.js
+++ src/index.js
@@ -54,7 +54,18 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  if (!validate(id))
+    return response.status(400).json({ error: "The id must be an uuid" });
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) return response.status(404).json({ error: "User not found" });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post("/users", (request, response) => {
